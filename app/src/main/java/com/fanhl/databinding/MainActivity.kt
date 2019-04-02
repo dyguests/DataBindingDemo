c40f161f0592f7f259b963730ef354d679de7c42
fanhl <fanhonglin@klicen.com>
2019-04-02 17:22:41 +0800

    BindingAdapter

--- app/src/main/java/com/fanhl/databinding/MainActivity.kt
+++ app/src/main/java/com/fanhl/databinding/MainActivity.kt
@@ -2,9 +2,14 @@ package com.fanhl.databinding
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
+import androidx.annotation.RestrictTo
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.BaseObservable
 import androidx.databinding.Bindable
+import androidx.databinding.BindingAdapter
+import androidx.databinding.BindingMethod
+import androidx.databinding.BindingMethods
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.MutableLiveData
 import com.fanhl.databinding.databinding.ActivityMainBinding
@@ -35,18 +40,31 @@ data class User(
 )
 
 open class Presenter {
-    private val TAG = Presenter::class.java.simpleName
-
     open fun onSaveClick(task: Task) {
         Log.d(TAG, "onSaveClick: $task")
+    }
+
+    companion object {
+        private val TAG = Presenter::class.java.simpleName
     }
 }
 
 class Task : BaseObservable() {
-    @get:Bindable
-    var firstName: String = ""
-        set(value) {
-            field = value
-            notifyPropertyChanged(BR.task)
-        }
+    // @get:Bindable
+    // var firstName: String = ""
+    //     set(value) {
+    //         field = value
+    //         notifyPropertyChanged(BR.task)
+    //     }
+}
+
+@RestrictTo(RestrictTo.Scope.LIBRARY)
+@BindingMethods(
+    BindingMethod(type = TextView::class, attribute = "android:paddingLeft", method = "setPaddingLeft")
+)
+object MyTextViewBindingAdapter {
+    @BindingAdapter("android:paddingLeft")
+    fun setPaddingLeft(view: TextView, padding: Int) {
+        view.setPadding(padding, padding, padding, padding)
+    }
 }
