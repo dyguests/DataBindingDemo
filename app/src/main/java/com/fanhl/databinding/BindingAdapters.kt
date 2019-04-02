9b6a18e9be3ba79ecebd1a4cd283900858ecf088
fanhl <fanhonglin@klicen.com>
2019-04-02 18:04:53 +0800

    BindingAdapters

--- app/src/main/java/com/fanhl/databinding/BindingAdapters.kt
+++ app/src/main/java/com/fanhl/databinding/BindingAdapters.kt
@@ -2,11 +2,13 @@ package com.fanhl.databinding
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import androidx.databinding.BindingMethod
+import androidx.databinding.BindingMethods
 
 // @RestrictTo(RestrictTo.Scope.LIBRARY)
-// @BindingMethods(
-//     BindingMethod(type = TextView::class, attribute = "android:paddingLeft", method = "setPaddingLeft")
-// )
+@BindingMethods(
+    BindingMethod(type = TextView::class, attribute = "android:paddingLeft", method = "setPaddingLeft")
+)
 object BindingAdapters {
     @BindingAdapter("android:paddingLeft")
     @JvmStatic
