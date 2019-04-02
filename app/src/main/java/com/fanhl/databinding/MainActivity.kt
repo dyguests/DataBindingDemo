5182303d6e9583ac55e95a6097e7f1e63c3e7e96
fanhl <fanhonglin@klicen.com>
2019-04-02 15:09:23 +0800

    ActivityMainBinding

--- app/src/main/java/com/fanhl/databinding/MainActivity.kt
+++ app/src/main/java/com/fanhl/databinding/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.fanhl.databinding
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.fanhl.databinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 }
+
+data class User(
+    val firstName: String
+)
